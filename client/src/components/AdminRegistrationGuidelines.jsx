6f01daa3ff395496ce12f87ec7363aa9d123fe54
Waad Zedan <waadzedan90@gmail.com>
2026-01-18 00:15:37 +0200

    update regestration

--- client/src/components/AdminRegistrationGuidelines.jsx
+++ client/src/components/AdminRegistrationGuidelines.jsx
@@ -36,7 +36,7 @@ const emptyLink = () => ({ label: "", url: "" });
 
 const deepClone = (x) => JSON.parse(JSON.stringify(x));
 
-// --- Icons (Inline SVGs for zero dependencies) ---
+// --- Icons (Inline SVGs for zero dependencies) 
 const Icons = {
   Save: (props) => <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}><path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z"/><polyline points="17 21 17 13 7 13 7 21"/><polyline points="7 3 7 8 15 8"/></svg>,
   Refresh: (props) => <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}><path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 12"/><path d="M3 3v9h9"/></svg>,
